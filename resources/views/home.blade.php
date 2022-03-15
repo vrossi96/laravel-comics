20b01dab6f1d6ca263e4d7083311442ef9ea04ef
vrossi96 <vrossi396@yahoo.it>
2022-03-15 16:31:23 +0100

    add comic cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,33 @@
+@php
+$comics = config('comics');
+@endphp
+
 @extends('layout.main')
 
 @section('title-name', 'Home')
 
 @section('main')
-<div class="container">
-   <div class="row">
-      <div class="col-6 offset-3">
-         HOME
+   <main>
+      <div class="container">
+         <div class="row">
+            <div class="col-12">
+               <div class="row">
+                  {{-- CARD COMIC --}}
+                  @forelse ($comics as $comic)
+                     <div class="col-2">
+                        <div role="button" class="comic-card">
+                           <div>
+                              <img class="d-block" src="{{ $comic['thumb'] }}" alt="{{ $comic['series'] }}">
+                           </div>
+                           <p>{{ $comic['series'] }}</p>
+                        </div>
+                     </div>
+                  @empty
+                     <h1 class="text-center">Nessun Fumetto selezionato</h1>
+                  @endforelse
+               </div>
+            </div>
+         </div>
       </div>
-   </div>
-</div>
+   </main>
 @endsection

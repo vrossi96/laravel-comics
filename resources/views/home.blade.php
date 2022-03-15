20f86d208aaad8de0d62da60f66f08d4c3069739
vrossi96 <vrossi396@yahoo.it>
2022-03-15 16:46:47 +0100

    add button and "label" in card section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,9 +8,10 @@ $comics = config('comics');
 
 @section('main')
    <main>
-      <div class="container">
+      <div class="card-section container position-relative">
          <div class="row">
             <div class="col-12">
+               {{-- CARD ROW --}}
                <div class="row">
                   {{-- CARD COMIC --}}
                   @forelse ($comics as $comic)
@@ -26,7 +27,19 @@ $comics = config('comics');
                      <h1 class="text-center">Nessun Fumetto selezionato</h1>
                   @endforelse
                </div>
+               {{-- BUTTON BOTTOM --}}
+               <div class="row">
+                  <div class="col-2 offset-5">
+                     <div class="text-uppercase dc-button" role="button">
+                        load more
+                     </div>
+                  </div>
+               </div>
             </div>
+         </div>
+         {{-- LABEL --}}
+         <div class="label">
+            <h3 class="text-uppercase">current series</h3>
          </div>
       </div>
    </main>

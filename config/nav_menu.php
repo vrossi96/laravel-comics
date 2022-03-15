327789f2c28e61bd1a3e4e5ee688e63168f4fe5e
vrossi96 <vrossi396@yahoo.it>
2022-03-15 15:59:06 +0100

    add header

---
+++ config/nav_menu.php
@@ -0,0 +1,13 @@
+<?php
+return [
+   ['text' => 'characters', 'route' => 'characters'],
+   ['text' => 'comic', 'route' => 'comic'],
+   ['text' => 'movies', 'route' => 'movies'],
+   ['text' => 'tv', 'route' => 'tv'],
+   ['text' => 'games', 'route' => 'games'],
+   ['text' => 'collectibles', 'route' => 'collectibles'],
+   ['text' => 'videos', 'route' => 'videos'],
+   ['text' => 'fans', 'route' => 'fans'],
+   ['text' => 'news', 'route' => 'news'],
+   ['text' => 'shop', 'route' => 'shop'],
+];
--- resources/views/layout/partials/header.blade.php
+++ resources/views/layout/partials/header.blade.php
@@ -1,6 +1,37 @@
+@php
+$nav_links = config('nav_menu');
+@endphp
+
 <header>
+   {{-- TOP NAV --}}
    <div class="upper-nav"></div>
+   {{-- NAV --}}
    <nav>
-      NAV
+      <div class="container">
+         <div class="row">
+            <div class="col-12">
+               <div class="d-flex align-content-center justify-content-between">
+                  {{-- LOGO LEFT --}}
+                  <div class="logo-nav">
+                     <a href="{{ route('home') }}">
+                        <img class="d-block py-3" src="{{ asset('images/dc-logo.png') }}" alt="DC Logo">
+                     </a>
+                  </div>
+                  {{-- NAV --}}
+                  <ul class="nav d-flex align-items-center">
+                     @foreach ($nav_links as $link)
+                        <li>
+                           <a class="text-uppercase" href="{{ $link['route'] }}">{{ $link['text'] }}</a>
+                        </li>
+                     @endforeach
+                  </ul>
+                  {{-- SEARCH RIGHT --}}
+                  <div class="search-nav d-flex align-items-center">
+                     <input class="search" type="text" placeholder="Search">
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
    </nav>
 </header>

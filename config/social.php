6cd00fb0d50995d1427104e1cf4e5395080ee330
vrossi96 <vrossi396@yahoo.it>
2022-03-15 21:13:26 +0100

    added bottom footer

---
+++ config/social.php
@@ -0,0 +1,8 @@
+<?php 
+return [
+   ['name' => 'Facebook', 'img' => 'footer-facebook.png'],
+   ['name' => 'Twitter', 'img' => 'footer-twitter.png'],
+   ['name' => 'YouTube', 'img' => 'footer-youtube.png'],
+   ['name' => 'Pinterest', 'img' => 'footer-pinterest.png'],
+   ['name' => 'Periscope', 'img' => 'footer-periscope.png'],
+];
--- resources/views/layout/partials/footer.blade.php
+++ resources/views/layout/partials/footer.blade.php
@@ -1,5 +1,7 @@
 @php
 $link_blocks = config('footer_menu');
+
+$socials = config('social');
 @endphp
 
 <footer>
@@ -34,4 +36,23 @@ $link_blocks = config('footer_menu');
       </div>
    </div>
    {{-- BOTTOM FOOTER --}}
+   <div class="bottom-footer">
+      <div class="container">
+         <div class="row">
+            <div class="col-12">
+               <div class="d-flex justify-content-between">
+                  <a class="d-inline-block btn-gray text-uppercase" href="#">Sign-up now!</a>
+                  <div class="social d-flex align-items-center">
+                     <p class="text-uppercase">Follow us</p>
+                     @foreach ($socials as $social)
+                        <a href="#">
+                           <img src="{{ asset('images/' . $social['img']) }}" alt="{{ $social['name'] }}">
+                        </a>
+                     @endforeach
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
 </footer>
